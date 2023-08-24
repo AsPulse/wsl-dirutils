6c8be92eb3cefa6886018a049c6871bc37f94ca9
AsPulse <aspulse.x@gmail.com>
2023-08-24 12:37:23 +0900

    Add colored

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::process::exit;
+use colored::Colorize;
 
 pub fn run() {
-    eprintln!("Usage: wsl-dirutils <cd|pwd>");
+    eprintln!("{}", "Usage: wsl-dirutils <cd|pwd>".red());
     exit(1);
 }
